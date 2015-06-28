b69eab22d9336e0877769c07e90ec8e1c3fe2cec
tmoreno <tmorenobernal@yahoo.es>
2015-06-28 18:23:40 +0200

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ router.get('/', function(req, res, next) {
 
 router.get('/author', authorController.credits);
 
-router.get('/quizes/question', quizController.question);
-
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
