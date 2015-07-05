72997cd2f0add19bca5e90beca0ece243eb8dd17
tmoreno <tmorenobernal@yahoo.es>
2015-07-05 16:26:41 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -45,3 +45,17 @@ exports.answer = function (req, res) {
             
     res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
+
+exports.new = function (req, res) {
+    var quiz = models.Quiz.build({pregunta: "Pregunta", respuesta: "Respuesta"});
+    
+    res.render('quizes/new', {quiz: quiz});
+};
+
+exports.create = function (req, res) {
+    var quiz = models.Quiz.build(req.body.quiz);
+    
+    quiz.save({fields: ["pregunta", "respuesta"]}).then(function () {
+        res.redirect('/quizes');
+    });
+};
